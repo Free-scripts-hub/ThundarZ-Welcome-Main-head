7ee0c27f4730fe63649e2d0c49443ae44de0f403
Free-scripts-hub <albieeeq@gmail.com>
2025-07-18 18:19:01 +0800

    Create ThundarZ.lua

---
+++ ThundarZ.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/WT9kQFZTDX/0", true))()
